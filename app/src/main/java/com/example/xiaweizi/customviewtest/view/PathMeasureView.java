0a5a0427d862f355d1a302976f902a1dc1b98c2b
xiaweizi <1012126908@qq.com>
2018-10-11 17:53:20 +0800

    [dev] 添加动画效果

--- app/src/main/java/com/example/xiaweizi/customviewtest/view/PathMeasureView.java
+++ app/src/main/java/com/example/xiaweizi/customviewtest/view/PathMeasureView.java
@@ -6,10 +6,14 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathMeasure;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.lang.ref.WeakReference;
 
 /**
  * <pre>
@@ -28,6 +32,7 @@ public class PathMeasureView extends View {
     private Path mDstPath;
     private PathMeasure mPathMeasure;
     private float mCurValue;
+    private MyHandler mHandler;
 
     private void initPaint() {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -47,6 +52,7 @@ public class PathMeasureView extends View {
 
     public PathMeasureView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mHandler = new MyHandler(this);
         initView(context, attrs, defStyle);
         setLayerType(LAYER_TYPE_SOFTWARE, null);
     }
@@ -63,28 +69,14 @@ public class PathMeasureView extends View {
         mPath.lineTo(50, 150);
         mPath.lineTo(50, 50);
         mPathMeasure = new PathMeasure(mPath, false);
-//        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1, 2);
-//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation) {
-//                mCurValue = (float) animation.getAnimatedValue();
-//                invalidate();
-//            }
-//        });
-//        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
-//        valueAnimator.setDuration(4000);
-//        valueAnimator.start();
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_UP:
-                if (mCurValue >= 2) {
-                    mCurValue = 0;
-                }
-                mCurValue += 0.1d;
-                invalidate();
+                mHandler.sendEmptyMessage(1);
                 break;
         }
         return true;
@@ -94,17 +86,43 @@ public class PathMeasureView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.translate(100, 100);
-        mDstPath.reset();
-        mCurValue = ((int) (mCurValue * 10)) / 10f;
-        Log.i(TAG, "onDraw: " + mCurValue);
+        Log.i(TAG, "onDraw1: " + mCurValue);
+        mCurValue = ((int) (mCurValue * 100)) / 100f;
         if (mCurValue < 1) {
             mPathMeasure.getSegment(0, mPathMeasure.getLength() * mCurValue, mDstPath, true);
-        } else if (mCurValue == 1.0) {
-            mPathMeasure.getSegment(0, mPathMeasure.getLength(), mDstPath, true);
-            mPathMeasure.nextContour();
+        } else if (mCurValue == 1.0 || mCurValue == 2.0) {
+            if (mCurValue == 2.0) {
+                mCurValue = 0;
+                mDstPath.reset();
+                mPathMeasure = new PathMeasure(mPath, false);
+            } else {
+                mPathMeasure.getSegment(0, mPathMeasure.getLength(), mDstPath, true);
+                mPathMeasure.nextContour();
+            }
+
         } else {
             mPathMeasure.getSegment(0, mPathMeasure.getLength() * (mCurValue - 1), mDstPath, true);
         }
         canvas.drawPath(mDstPath, mPaint);
+    }
+
+    static class MyHandler extends Handler {
+        WeakReference<PathMeasureView> mView;
+    
+        MyHandler(PathMeasureView view) {
+            mView = new WeakReference<>(view);
+        }
+    
+        @Override
+        public void handleMessage(Message msg) {
+            PathMeasureView theView = mView.get();
+            if (theView == null || theView.getContext() == null) {
+                return;
+            }
+            // 消息处理
+            theView.mCurValue += 0.1d;
+            theView.invalidate();
+            theView.mHandler.sendEmptyMessageDelayed(1, 50);
+        }
     }
 }
