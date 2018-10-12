1267d498e7e0240b4685d1a1145f7d27a6523821
xiaweizi <1012126908@qq.com>
2018-10-12 13:36:15 +0800

    [dev] 适量图动画效果添加

--- app/src/main/java/com/example/xiaweizi/customviewtest/VectorActivity.java
+++ app/src/main/java/com/example/xiaweizi/customviewtest/VectorActivity.java
@@ -1,13 +1,30 @@
 package com.example.xiaweizi.customviewtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class VectorActivity extends AppCompatActivity {
+
+    private ImageView mIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vector);
+        mIv = findViewById(R.id.iv_svg);
+        mIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable drawable = mIv.getDrawable();
+                if (drawable instanceof Animatable) {
+                    ((Animatable) drawable).start();
+                }
+            }
+        });
+
     }
 }
